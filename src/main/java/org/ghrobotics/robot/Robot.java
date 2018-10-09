5aac7a53042ab9c8208c8e7c4c3989a88b875fe6
Prateek Machiraju <prateek.machiraju@gmail.com>
2018-10-08 21:06:30 -0400

    Basic Robot Template

--- src/main/java/org/ghrobotics/robot/Robot.java
+++ src/main/java/org/ghrobotics/robot/Robot.java
@@ -1,38 +1,62 @@
 package org.ghrobotics.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
 
+    // Motor IDs
+    private final int kLeftMasterId = 1;
+    private final int kLeftSlaveId = 2;
+    private final int kRightMasterId = 3;
+    private final int kRightSlaveId = 4;
+
+    // Xbox Controller Port
+    private final int kXboxControllerPort = 0;
+
+    // Motor Controllers
+    private final TalonSRX leftMaster = new TalonSRX(kLeftMasterId);
+    private final TalonSRX leftSlave = new TalonSRX(kLeftSlaveId);
+    private final TalonSRX rightMaster = new TalonSRX(kRightMasterId);
+    private final TalonSRX rightSlave = new TalonSRX(kRightSlaveId);
+
+    // Xbox Controller
+    private final XboxController controller = new XboxController(kXboxControllerPort);
+
     @Override
     public void robotInit() {
-    }
+        // Followers
+        leftSlave.follow(leftMaster);
+        rightSlave.follow(rightMaster);
 
-    @Override
-    public void disabledInit() {
-    }
-
-    @Override
-    public void autonomousInit() {
-    }
-
-    @Override
-    public void teleopInit() {
-    }
-
-    @Override
-    public void robotPeriodic() {
-    }
-
-    @Override
-    public void disabledPeriodic() {
+        // Motor Inversion
+        leftMaster.setInverted(false);
+        leftSlave.setInverted(false);
+        rightMaster.setInverted(true);
+        rightSlave.setInverted(true);
     }
 
     @Override
     public void autonomousPeriodic() {
+        // Add your autonomous code here
     }
 
     @Override
     public void teleopPeriodic() {
+        // Control robot using two sticks on Xbox controller
+        drive(-controller.getY(GenericHID.Hand.kLeft), -controller.getY(GenericHID.Hand.kRight));
+    }
+
+    /**
+     * Sends an output to the drive motors
+     * @param left The output to the left side of the drivetrain. Should be a value between -1 and 1
+     * @param right The output to the right side of the drivetrain. Should be a value between -1 and 1
+     */
+    private void drive(double left, double right) {
+        leftMaster.set(ControlMode.PercentOutput, left);
+        rightMaster.set(ControlMode.PercentOutput, right);
     }
 }
