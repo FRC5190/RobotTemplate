07daa452425466c33ac3c83914bc4519f65022bc
Vijay Machiraju <vijayma@microsoft.com>
2018-11-03 08:32:13 -0400

    More basic template

--- src/main/java/org/ghrobotics/robot/Robot.java
+++ src/main/java/org/ghrobotics/robot/Robot.java
@@ -8,55 +8,33 @@ import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
 
-    // Motor IDs
-    private final int kLeftMasterId = 1;
-    private final int kLeftSlaveId = 2;
-    private final int kRightMasterId = 3;
-    private final int kRightSlaveId = 4;
-
-    // Xbox Controller Port
-    private final int kXboxControllerPort = 0;
-
-    // Motor Controllers
-    private final TalonSRX leftMaster = new TalonSRX(kLeftMasterId);
-    private final TalonSRX leftSlave = new TalonSRX(kLeftSlaveId);
-    private final TalonSRX rightMaster = new TalonSRX(kRightMasterId);
-    private final TalonSRX rightSlave = new TalonSRX(kRightSlaveId);
-
-    // Xbox Controller
-    private final XboxController controller = new XboxController(kXboxControllerPort);
-
     @Override
     public void robotInit() {
-        // Followers
-        leftSlave.follow(leftMaster);
-        rightSlave.follow(rightMaster);
+        // Add your robot initialization code here
+    }
 
-        // Motor Inversion
-        leftMaster.setInverted(false);
-        leftSlave.setInverted(false);
-        rightMaster.setInverted(true);
-        rightSlave.setInverted(true);
+    @Override
+    public void robotPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit() {
+        // Add your teleop initialization here
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        // Add your teleop code here
+    }
+
+    @Override
+    public void autonomousInit() {
+        // Add your autonomous initialization here
     }
 
     @Override
     public void autonomousPeriodic() {
         // Add your autonomous code here
-    }
-
-    @Override
-    public void teleopPeriodic() {
-        // Control robot using two sticks on Xbox controller
-        drive(-controller.getY(GenericHID.Hand.kLeft), -controller.getY(GenericHID.Hand.kRight));
-    }
-
-    /**
-     * Sends an output to the drive motors
-     * @param left The output to the left side of the drivetrain. Should be a value between -1 and 1
-     * @param right The output to the right side of the drivetrain. Should be a value between -1 and 1
-     */
-    private void drive(double left, double right) {
-        leftMaster.set(ControlMode.PercentOutput, left);
-        rightMaster.set(ControlMode.PercentOutput, right);
     }
 }
